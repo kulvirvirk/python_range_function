eb586ea0744cb5db5ba395c6bb82fc38328af218
kulvirvirk <kvirkdev@gmail.com>
2020-02-23 04:20:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# print a numbers from 1 to 5 using a range() function
+# hint: in range(start, stop, step) function:
+# - start is inclusive 
+# - stop is exclusive 
+# - step is optional  
+
+#note: print() here uses multiple arguments to format
+#print() function concatnates both args string and int and also adds a single space between them.
+for number in range(1,6):
+  print('A number:', number)
+
+print(type(number))
+#
